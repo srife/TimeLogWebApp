bdd2d413d8237ff1aaf306a7c0e50f250af95ecb
tom <tom@srife.net>
2019-08-30 11:53:24 -0400

    minor formatting changes to activites index.

--- TimeLog/wwwroot/js/site.js
+++ TimeLog/wwwroot/js/site.js
@@ -3,3 +3,15 @@
 
 // Write your Javascript code.
 
+$(document).ready(function () {
+    $(".invoice-statement").show($("#ActivityEntity_Billable").val());
+
+    $("#ActivityEntity_Billable").change(function () {
+        //alert($("#ActivityEntity_Billable").val());
+        if (this.value === true) {
+            $(".invoice-statement").show();
+        } else {
+            $(".invoice-statement").css("display", "");
+        }
+    });
+});
