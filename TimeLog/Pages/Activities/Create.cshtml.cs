39720715a2da65ec412a8c0f74f76481cd805639
tom <tom@srife.net>
2019-09-07 16:33:01 -0400

    remove unused using

--- TimeLog/Pages/Activities/Create.cshtml.cs
+++ TimeLog/Pages/Activities/Create.cshtml.cs
@@ -1,9 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 using TimeLog.Extensions;
 using TimeLog.Models;
 
