86fb357a5d55328f2e73662939a24e0671f41620
tom <tom@srife.net>
2020-04-02 11:53:41 -0400

    report modifications

--- TimeLog/Pages/Activities/Report.cshtml.cs
+++ TimeLog/Pages/Activities/Report.cshtml.cs
@@ -31,7 +31,7 @@ namespace TimeLog.Pages.Activities
 
         public async Task<IActionResult> OnGetAsync()
         {
-            StartTime = DateTimeOffset.UtcNow;
+            StartTime = DateTimeOffset.UtcNow.ToLocalTime();
 
             //remove hours, minutes, seconds, milliseconds
             StartTime = StartTime.AddMilliseconds(-StartTime.Millisecond).AddSeconds(-StartTime.Second).AddMinutes(-StartTime.Minute).AddHours(-StartTime.Hour);
@@ -42,7 +42,7 @@ namespace TimeLog.Pages.Activities
             //if Sunday subtract 6 to get the previous Monday, otherwise subtract the day of week index plus one
             var daysToSubtract = (dayOfWeek) == DayOfWeek.Sunday ? -6 : -(int)dayOfWeek + 1;
             StartTime = StartTime.AddDays(daysToSubtract);
-            EndTime = StartTime.AddDays(6);
+            EndTime = StartTime.AddDays(6).AddHours(23).AddMinutes(59).AddSeconds(59).AddMilliseconds(999);
 
             Rate = 37.5M;
             var p = new object[]
@@ -66,12 +66,8 @@ namespace TimeLog.Pages.Activities
                 .TagWith("my second statement")
                 .ToListAsync();
 
-            //PopulateActivityTypesDropDownList(_context);
-            //PopulateClientDropDownList(_context);
-            //PopulateProjectsDropDownList(_context);
-            //PopulateLocationDropDownList(_context);
-
             ActivityEntities = await PopulateActivityEntities(StartTime, EndTime);
+
             return Page();
         }
 
@@ -79,7 +75,7 @@ namespace TimeLog.Pages.Activities
         {
             LastWeek = true;
 
-            StartTime = DateTimeOffset.UtcNow;
+            StartTime = DateTimeOffset.UtcNow.ToLocalTime();
 
             //remove hours, minutes, seconds, milliseconds
             StartTime = StartTime.AddMilliseconds(-StartTime.Millisecond).AddSeconds(-StartTime.Second).AddMinutes(-StartTime.Minute).AddHours(-StartTime.Hour);
@@ -95,7 +91,7 @@ namespace TimeLog.Pages.Activities
             //if Sunday subtract 6 to get the previous Monday, otherwise subtract the day of week index plus one
             var daysToSubtract = (dayOfWeek) == DayOfWeek.Sunday ? -6 : -(int)dayOfWeek + 1;
             StartTime = StartTime.AddDays(daysToSubtract);
-            EndTime = StartTime.AddDays(6);
+            EndTime = StartTime.AddDays(6).AddHours(23).AddMinutes(59).AddSeconds(59).AddMilliseconds(999);
 
             Rate = 37.5M;
             var p = new object[]
@@ -119,10 +115,6 @@ namespace TimeLog.Pages.Activities
                 .TagWith("my second statement")
                 .ToListAsync();
 
-            //PopulateActivityTypesDropDownList(_context);
-            //PopulateClientDropDownList(_context);
-            //PopulateProjectsDropDownList(_context);
-            //PopulateLocationDropDownList(_context);
             ActivityEntities = await PopulateActivityEntities(StartTime, EndTime);
             return Page();
         }
@@ -131,7 +123,7 @@ namespace TimeLog.Pages.Activities
         {
             LastWeek = true;
 
-            StartTime = DateTimeOffset.UtcNow;
+            StartTime = DateTimeOffset.UtcNow.ToLocalTime();
 
             //remove hours, minutes, seconds, milliseconds
             StartTime = StartTime.AddMilliseconds(-StartTime.Millisecond).AddSeconds(-StartTime.Second).AddMinutes(-StartTime.Minute).AddHours(-StartTime.Hour);
@@ -147,7 +139,7 @@ namespace TimeLog.Pages.Activities
             //if Sunday subtract 6 to get the previous Monday, otherwise subtract the day of week index plus one
             var daysToSubtract = (dayOfWeek) == DayOfWeek.Sunday ? -6 : -(int)dayOfWeek + 1;
             StartTime = StartTime.AddDays(daysToSubtract);
-            EndTime = StartTime.AddDays(6);
+            EndTime = StartTime.AddDays(6).AddHours(23).AddMinutes(59).AddSeconds(59).AddMilliseconds(999);
 
             Rate = 37.5M;
             var p = new object[]
@@ -171,15 +163,107 @@ namespace TimeLog.Pages.Activities
                 .TagWith("my second statement")
                 .ToListAsync();
 
-            //PopulateActivityTypesDropDownList(_context);
-            //PopulateClientDropDownList(_context);
-            //PopulateProjectsDropDownList(_context);
-            //PopulateLocationDropDownList(_context);
             ActivityEntities = await PopulateActivityEntities(StartTime, EndTime);
             return Page();
         }
 
-        public TimeSpan MyConvert(long? seconds)
+        public async Task<IActionResult> OnPostThreeWeeksAgo()
+        {
+            LastWeek = true;
+
+            StartTime = DateTimeOffset.UtcNow.ToLocalTime();
+
+            //remove hours, minutes, seconds, milliseconds
+            StartTime = StartTime.AddMilliseconds(-StartTime.Millisecond).AddSeconds(-StartTime.Second).AddMinutes(-StartTime.Minute).AddHours(-StartTime.Hour);
+
+            if (LastWeek)
+            {
+                StartTime = StartTime.AddDays(-21);
+            }
+
+            //get current day of week
+            var dayOfWeek = StartTime.DayOfWeek;
+
+            //if Sunday subtract 6 to get the previous Monday, otherwise subtract the day of week index plus one
+            var daysToSubtract = (dayOfWeek) == DayOfWeek.Sunday ? -6 : -(int)dayOfWeek + 1;
+            StartTime = StartTime.AddDays(daysToSubtract);
+            EndTime = StartTime.AddDays(6).AddHours(23).AddMinutes(59).AddSeconds(59).AddMilliseconds(999);
+
+            Rate = 37.5M;
+            var p = new object[]
+            {
+                new SqlParameter("@p0", SqlDbType.DateTimeOffset, 7) {Value = StartTime},
+                new SqlParameter("@p1", SqlDbType.DateTimeOffset, 7) {Value = EndTime},
+                new SqlParameter("@p2", SqlDbType.Money, 4) {Value = Rate},
+                new SqlParameter("@p3", SqlDbType.Bit, 1) {Value = true}
+            };
+
+            Clients = await _context.Clients.ToListAsync();
+            Projects = await _context.Projects.ToListAsync();
+            Report = await _context
+                .Report
+                .FromSql("execute sp_Report @p0, @p1, @p2, @p3", p)
+                .TagWith("my statement")
+                .ToListAsync();
+
+            ReportDetailsByDay = await _context.ReportDetailsByDay
+                .FromSql("execute sp_ReportDetailsByDay @p0, @p1, @p2, @p3", p)
+                .TagWith("my second statement")
+                .ToListAsync();
+
+            ActivityEntities = await PopulateActivityEntities(StartTime, EndTime);
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostFourWeeksAgo()
+        {
+            LastWeek = true;
+
+            StartTime = DateTimeOffset.UtcNow.ToLocalTime();
+
+            //remove hours, minutes, seconds, milliseconds
+            StartTime = StartTime.AddMilliseconds(-StartTime.Millisecond).AddSeconds(-StartTime.Second).AddMinutes(-StartTime.Minute).AddHours(-StartTime.Hour);
+
+            if (LastWeek)
+            {
+                StartTime = StartTime.AddDays(-28);
+            }
+
+            //get current day of week
+            var dayOfWeek = StartTime.DayOfWeek;
+
+            //if Sunday subtract 6 to get the previous Monday, otherwise subtract the day of week index plus one
+            var daysToSubtract = (dayOfWeek) == DayOfWeek.Sunday ? -6 : -(int)dayOfWeek + 1;
+            StartTime = StartTime.AddDays(daysToSubtract);
+            EndTime = StartTime.AddDays(6).AddHours(23).AddMinutes(59).AddSeconds(59).AddMilliseconds(999);
+
+            Rate = 37.5M;
+            var p = new object[]
+            {
+                new SqlParameter("@p0", SqlDbType.DateTimeOffset, 7) {Value = StartTime},
+                new SqlParameter("@p1", SqlDbType.DateTimeOffset, 7) {Value = EndTime},
+                new SqlParameter("@p2", SqlDbType.Money, 4) {Value = Rate},
+                new SqlParameter("@p3", SqlDbType.Bit, 1) {Value = true}
+            };
+
+            Clients = await _context.Clients.ToListAsync();
+            Projects = await _context.Projects.ToListAsync();
+            Report = await _context
+                .Report
+                .FromSql("execute sp_Report @p0, @p1, @p2, @p3", p)
+                .TagWith("my statement")
+                .ToListAsync();
+
+            ReportDetailsByDay = await _context.ReportDetailsByDay
+                .FromSql("execute sp_ReportDetailsByDay @p0, @p1, @p2, @p3", p)
+                .TagWith("my second statement")
+                .ToListAsync();
+
+            ActivityEntities = await PopulateActivityEntities(StartTime, EndTime);
+            return Page();
+        }
+
+        public TimeSpan ConvertDurationToTimeSpan(long? seconds)
         {
             if (seconds is null)
             {
@@ -188,6 +272,25 @@ namespace TimeLog.Pages.Activities
             else
             {
                 return TimeSpan.FromSeconds(Convert.ToInt64(seconds));
+            }
+        }
+
+        public decimal ConvertDurationToDecimal(double? seconds)
+        {
+            if (seconds is null)
+            {
+                return 0;
+            }
+            else
+            {
+                var firstvalue = (decimal)seconds;
+                var secondvalue = (firstvalue / 900);
+                var thirdvalue = Math.Ceiling(secondvalue);
+                var adjustedSeconds = thirdvalue * 900;
+                var result = Math.Round(adjustedSeconds / 60.0m / 60.0m, 2);
+                return result;
+
+                //return Math.Round((double)seconds / 60.0m / 60.0m, 2);
             }
         }
 
